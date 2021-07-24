e394ea8d9716f39043fa603982994a624b077ce7
Arthur Puyou <arthur@puyou.fr>
2021-07-24 19:34:07 +0200

    Add prettier

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,9 +1,9 @@
-import { primarySaleMint } from "../generated/Contract/Contract"
-import { Ticket } from "../generated/schema"
+import { primarySaleMint } from "../generated/Contract/Contract";
+import { Ticket } from "../generated/schema";
 
 export function handleprimarySaleMint(event: primarySaleMint): void {
-  let ticket = new Ticket(event.transaction.hash.toHex())
-  ticket.event = event.params.eventAddress.toHex()
-  ticket.owner = event.params.destinationAddress
-  ticket.save()
+  let ticket = new Ticket(event.transaction.hash.toHex());
+  ticket.event = event.params.eventAddress.toHex();
+  ticket.owner = event.params.destinationAddress;
+  ticket.save();
 }
